5342f0319f896350c625d5b68beaa85ac2414b54
cloudbyteelias <cloudbyteelias@gmail.com>
2020-06-12 23:31:34 +0000

    Creating log

--- SL.c
+++ SL.c
@@ -3,19 +3,21 @@
 #include <termios.h> 
 #include <unistd.h>  
 #include <errno.h>   
-	
+#include <time.h>
+
+
     void main(void){
           
       struct termios serial;	                      
       int fd;
-      const char port[] = "/dev/ttyUSB0";
+      const char port[] = "/dev/ttyUSB0";      // Edit for other port 
       fd = open(port,O_RDWR | O_NOCTTY);			//O_RDWR Read and write,					
         if(fd == -1)						
         	   printf("\n  Error Fail open port %s",port);
         else
              printf("\n  %s Opened Successfully", port);
 
-		if(tcgetattr(fd, &serial) < 0) {printf("ERROR IN C_FLAG CONFIG %s",port)}
+		if(tcgetattr(fd, &serial) < 0) {printf("ERROR IN C_FLAG CONFIG %s",port);}
 		
     tcgetattr(fd, &serial);	
 		cfsetispeed(&serial,B9600); 
@@ -32,30 +34,17 @@
 		serial.c_cc[VMIN] = 10; 
 		serial.c_cc[VTIME] = 0; 
 		
-    if(cfsetispeed(&serial, B9600) < 0 || cfsetospeed(&serial, B9600) < 0) {printf("SPEED ERROR IN %s",port)};
-    if(tcsetattr(fd, TCSAFLUSH, &serial) < 0) {printf(" %s ERROR TO SET attributes",port)}else{printf("%s Read Okay",port)};
-
-
-
-
-
-
-
-    //if((tcsetattr(fd,TCSANOW,&serial)) != 0) 
-		    //printf("\n  ERROR ! in Setting attributes");
-		//else
-             //printf("read port");
-
-		/tcflush(fd, TCIFLUSH);   
-    ////////////////////////////////////////////////////////
+    if(cfsetispeed(&serial, B9600) < 0 || cfsetospeed(&serial, B9600) < 0) {printf("SPEED ERROR IN %s",port);}
+    if(tcsetattr(fd, TCSAFLUSH, &serial) < 0) {printf(" %s ERROR TO SET attributes",port);}else{printf("%s Read Okay",port);}
 		
     char read_buffer[32];   
-		unsigned bytes_read = 0;    
+		int  bytes_read = 0;    
  		int i = 0;
 
 		bytes_read = read(fd,&read_buffer,32); 
-		for(i=0;i<bytes_read;i++)	 
-		    printf("%d",read_buffer[i]);
+		
+    for(i=0;i<bytes_read;i++)	 
+		    printf("%c",read_buffer[i]);
 
 		close(fd); 
 
