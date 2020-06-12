8d0c0ebe1d792c1d215f32781c575fc64216e900
cloudbyteelias <cloudbyteelias@gmail.com>
2020-06-12 23:09:06 +0000

    Error in inline IF

--- SL.c
+++ SL.c
@@ -9,15 +9,15 @@
       struct termios serial;	                      
       int fd;
       const char port[] = "/dev/ttyUSB0";
-      fd = open("port,O_RDWR | O_NOCTTY);			//O_RDWR Read and write,					
+      fd = open(port,O_RDWR | O_NOCTTY);			//O_RDWR Read and write,					
         if(fd == -1)						
-        	   printf("\n  Error Fail open port %c",port);
+        	   printf("\n  Error Fail open port %s",port);
         else
-             printf("\n  %c Opened Successfully", port);
+             printf("\n  %s Opened Successfully", port);
 
+		if(tcgetattr(fd, &serial) < 0) {printf("ERROR IN C_FLAG CONFIG %s",port)}
 		
-        
-		tcgetattr(fd, &serial);	
+    tcgetattr(fd, &serial);	
 		cfsetispeed(&serial,B9600); 
 		cfsetospeed(&serial,B9600); 
 		serial.c_cflag &= ~PARENB;   
@@ -32,12 +32,21 @@
 		serial.c_cc[VMIN] = 10; 
 		serial.c_cc[VTIME] = 0; 
 		
-    if((tcsetattr(fd,TCSANOW,&serial)) != 0) 
-		    printf("\n  ERROR ! in Setting attributes");
-		else
-             puts("read port");
+    if(cfsetispeed(&serial, B9600) < 0 || cfsetospeed(&serial, B9600) < 0) {printf("SPEED ERROR IN %s",port)};
+    if(tcsetattr(fd, TCSAFLUSH, &serial) < 0) {printf(" %s ERROR TO SET attributes",port)}else{printf("%s Read Okay",port)};
 
-		tcflush(fd, TCIFLUSH);   
+
+
+
+
+
+
+    //if((tcsetattr(fd,TCSANOW,&serial)) != 0) 
+		    //printf("\n  ERROR ! in Setting attributes");
+		//else
+             //printf("read port");
+
+		/tcflush(fd, TCIFLUSH);   
     ////////////////////////////////////////////////////////
 		
     char read_buffer[32];   
@@ -46,7 +55,7 @@
 
 		bytes_read = read(fd,&read_buffer,32); 
 		for(i=0;i<bytes_read;i++)	 
-		    printf("%c",read_buffer[i]);
+		    printf("%d",read_buffer[i]);
 
 		close(fd); 
 
