6ab0054afccfae3669f503c9a800e433f64f6b9e
cloudbyteelias <cloudbyteelias@gmail.com>
2020-06-12 22:54:47 +0000

    Alter struct

--- SL.c
+++ SL.c
@@ -5,41 +5,43 @@
 #include <errno.h>   
 	
     void main(void){
-    	
-        	int fd;
-        	const char port[] = "/dev/ttyUSB0";
-          fd = open("port,O_RDWR | O_NOCTTY);								
-        	if(fd == -1)						
-            	   printf("\n  Error! in Opening ttyUSB0  ");
-        	else
-            	   printf("\n  ttyUSB0 Opened Successfully ");
+          
+      struct termios serial;	                      
+      int fd;
+      const char port[] = "/dev/ttyUSB0";
+      fd = open("port,O_RDWR | O_NOCTTY);			//O_RDWR Read and write,					
+        if(fd == -1)						
+        	   printf("\n  Error Fail open port %c",port);
+        else
+             printf("\n  %c Opened Successfully", port);
 
 		
         
-    struct termios SerialPortSettings;	                      
-		tcgetattr(fd, &SerialPortSettings);	
-		cfsetispeed(&SerialPortSettings,B9600); 
-		cfsetospeed(&SerialPortSettings,B9600); 
-		SerialPortSettings.c_cflag &= ~PARENB;   
-		SerialPortSettings.c_cflag &= ~CSTOPB;  
-		SerialPortSettings.c_cflag &= ~CSIZE;	 
-		SerialPortSettings.c_cflag |=  CS8;      
-		SerialPortSettings.c_cflag &= ~CRTSCTS;                          
-		SerialPortSettings.c_cflag |= CREAD | CLOCAL; 
-		SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          
-		SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);                           
-		SerialPortSettings.c_oflag &= ~OPOST;
-		SerialPortSettings.c_cc[VMIN] = 10; 
-		SerialPortSettings.c_cc[VTIME] = 0; 
-		if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) 
+		tcgetattr(fd, &serial);	
+		cfsetispeed(&serial,B9600); 
+		cfsetospeed(&serial,B9600); 
+		serial.c_cflag &= ~PARENB;   
+		serial.c_cflag &= ~CSTOPB;  
+		serial.c_cflag &= ~CSIZE;	 
+		serial.c_cflag |=  CS8;      
+		serial.c_cflag &= ~CRTSCTS;                          
+		serial.c_cflag |= CREAD | CLOCAL; 
+		serial.c_iflag &= ~(IXON | IXOFF | IXANY);          
+		serial.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);                           
+		serial.c_oflag &= ~OPOST;
+		serial.c_cc[VMIN] = 10; 
+		serial.c_cc[VTIME] = 0; 
+		
+    if((tcsetattr(fd,TCSANOW,&serial)) != 0) 
 		    printf("\n  ERROR ! in Setting attributes");
 		else
              puts("read port");
 
 		tcflush(fd, TCIFLUSH);   
-
-		char read_buffer[32];   
-		int  bytes_read = 0;    
+    ////////////////////////////////////////////////////////
+		
+    char read_buffer[32];   
+		unsigned bytes_read = 0;    
  		int i = 0;
 
 		bytes_read = read(fd,&read_buffer,32); 
